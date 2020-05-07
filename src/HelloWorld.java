4fab91b42e985aeedca01370021bbb9244313b2c
Andrei <109a@mail.ru>
2020-05-07 21:41:23 +0300

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * HelloWorld
+ *
+ * @author "Andrei Prokofiev"
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
