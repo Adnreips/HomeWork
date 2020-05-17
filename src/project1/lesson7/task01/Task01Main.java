fac1d7ac20b255cdde9976b5118f3bf5b279f5d8
Andrei <109a@mail.ru>
2020-05-17 18:02:50 +0300

    Готовое решение

--- src/project1/lesson7/task01/Task01Main.java
+++ src/project1/lesson7/task01/Task01Main.java
@@ -16,8 +16,8 @@ public class Task01Main {
     public static void main(String[] args) throws Exception {
 
         Worker worker = new Worker();
-//        Integer[] array = worker.prepareArray();
-        Integer[] array = {2, 3, 4, 5};
+        Integer[] array = worker.prepareArray();
+//        Integer[] array = {2, 3, 4, 5};
                 System.out.println(Arrays.toString(array));
         Long start1 = System.currentTimeMillis();
         for (int i = 0; i < array.length; i++) {
@@ -25,6 +25,7 @@ public class Task01Main {
         }
         Long stop1 = System.currentTimeMillis();
         Long time1 = stop1 - start1;
+        System.out.println("====================");
         Long start = System.currentTimeMillis();
         Arrays.stream(array).
                 parallel().
@@ -37,6 +38,5 @@ public class Task01Main {
         System.out.printf("решение с использованием многопоточности, %d мс %n", time);
         System.out.printf("решение без использования многопоточности, %d мс", time1);
         hashMap.clear();
-
     }
 }
