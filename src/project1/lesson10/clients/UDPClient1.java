9246cb3caed801c1283bc53dd198b146f8069c5e
Andrei <109a@mail.ru>
2020-05-27 18:26:58 +0300

    Готовое задание №10

---
+++ src/project1/lesson10/clients/UDPClient1.java
@@ -0,0 +1,50 @@
+package src.project1.lesson10.clients;
+
+import src.project1.lesson10.Listener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/**
+ * UDPClient1
+ * Класс создает, отправляет и принимает
+ * сообщения от других пользователей.
+ *
+ * @author "Andrei Prokofiev"
+ */
+public class UDPClient1 {
+    public static void main(String args[]) throws Exception {
+
+        int serverPort = 9876;
+
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+
+        byte[] sendData = new byte[1000];
+        byte[] receiveData = new byte[1000];
+        String sentence = "null";
+        String userName = "Ivan";
+
+        try (DatagramSocket clientSocket = new DatagramSocket();
+             BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        ) {
+            Listener listener = new Listener(clientSocket);
+            listener.start();
+
+            sendData = userName.getBytes();
+            DatagramPacket sendPacketPort = new DatagramPacket(sendData, sendData.length, IPAddress, serverPort);
+            clientSocket.send(sendPacketPort);
+
+            while (!(sentence = inFromUser.readLine()).equals("quit")) {
+                sendData = sentence.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, serverPort);
+                clientSocket.send(sendPacket);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/project1/lesson10/server/UDPServer.java
@@ -0,0 +1,63 @@
+package src.project1.lesson10.server;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.*;
+
+/**
+ * UDPServer
+ * Класс представляет собой сервер, который принимает сообщение от пользователей
+ * и отправляет его всем подключенным пользователям.
+ *
+ * @author "Andrei Prokofiev"
+ */
+public class UDPServer {
+    public static void main(String[] args) throws Exception {
+
+        int serverPort = 9876;
+        List<Integer> ports = new ArrayList();
+        Map<Integer, String> userInfo = new HashMap<>();
+
+        try (DatagramSocket serverSocket = new DatagramSocket(serverPort)) {
+
+            while (true) {
+                byte[] receiveData = new byte[1000];
+                byte[] sendData = new byte[1000];
+
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+                serverSocket.receive(receivePacket);
+
+                String sentence = new String(receivePacket.getData()).trim();
+
+                InetAddress IPAddress = receivePacket.getAddress();
+
+                int port = receivePacket.getPort();
+
+                if (userInfo.containsKey(port)) {
+
+                    String nameUser = userInfo.get(port);
+
+                    String toSend = new String(nameUser + ": " + sentence);
+
+                    sendData = toSend.getBytes();
+
+                    Set<Integer> set = userInfo.keySet();
+                    Iterator iterator = set.iterator();
+                    System.out.println(toSend);
+
+                    while (iterator.hasNext()) {
+                        DatagramPacket sendPacket =
+                                new DatagramPacket(sendData, sendData.length, IPAddress, (Integer) iterator.next());
+                        serverSocket.send(sendPacket);
+                    }
+                } else {
+                    userInfo.put(port, sentence);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
