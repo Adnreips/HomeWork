aca8b4d0abde6e5141bca44de713eb2a41a859aa
Andrei <109a@mail.ru>
2020-06-04 16:01:50 +0300

    Готовое решение задачи 12

---
+++ src/project1/lesson12/Metaspace.java
@@ -0,0 +1,32 @@
+package src.project1.lesson12;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+
+/**
+ * Metaspace
+ * Программа демонстрирует ситуацию, когда ее выполнение
+ * приводит к ошибке в Metaspace.
+ *
+ * Для работы программы необходимо установить размер
+ * Metaspace: -XX:MaxMetaspaceSize=512m.
+ *
+ * Также необходимо подключить библиотеку: -javassist.jar.
+ *
+ * @author "Andrei Prokofiev"
+ */
+public class Metaspace {
+    public static void main(String[] args) throws CannotCompileException {
+
+        ClassPool pool = ClassPool.getDefault();
+        int i = 0;
+
+        while (true) {
+
+            Class cc = pool.makeClass("MyClass" + i).toClass();
+
+            i++;
+        }
+    }
+}
+
